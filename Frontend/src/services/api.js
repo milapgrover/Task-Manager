5266e473886c578b41914ea93a18a84b7d0ba95e
milap1234 <milapgrover27@gmail.com>
2026-05-03 20:24:32 +0530

    frontend ready for deploy

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/token";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use((config) => {
